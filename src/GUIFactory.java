be79e3e2e203e20e44414af01f9a50bad3a58680
evefxx <eve_jive@hotmail.com>
2023-10-02 15:20:38 +0700

    Initial commit

---
+++ src/GUIFactory.java
@@ -0,0 +1,6 @@
+
+public interface GUIFactory {
+    Button createButton();
+    Menu createMenu();
+	
+}
---
+++ src/MacButton.java
@@ -0,0 +1,7 @@
+
+public class MacButton implements Button {
+    public void render() {
+        System.out.println("Mac Button");
+    }
+}
+
